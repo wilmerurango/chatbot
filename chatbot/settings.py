0b78a20df0367ce8e40c16507be07fc1ebd8dcef
wilmerurango <wildau@hotmail.com>
2022-11-15 15:21:05 -0500

    se modifico otra vez el settings en database

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -77,19 +77,18 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# import dj_database_url
-# from decouple import config
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=config('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+import dj_database_url
+from decouple import config
 
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
