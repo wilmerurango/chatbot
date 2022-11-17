4c1b439f6e9faea6eaa6dd25ccab5001f27e7a06
wilmerurango <wildau@hotmail.com>
2022-11-17 09:27:42 -0500

    se actualizo la fomra de mostrar los recursos

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR,'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'staticfiles'),
+)
 
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
