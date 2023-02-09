1f335f43166a69d946f546d8ed0cb7e52440694b
wilmerurango <wildau@hotmail.com>
2023-02-08 22:10:47 -0500

    eee

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -125,19 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'staticfiles'),
-# ]
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
@@ -148,6 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'appbot.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
  
 
@@ -107,5 +107,5 @@ urlpatterns = [
 ]
 
 # if settings.DEBUG == False:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
