d36f806bea8144a9c9792195fc4a305414a28f99
wilmerurango <wildau@hotmail.com>
2023-02-12 19:09:49 -0500

    copia

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
 
 
 
@@ -135,7 +135,7 @@ LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
